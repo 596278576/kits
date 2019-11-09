9489d109f099256614f57ce70da6baa8023522fd
锅 <596278576@qq.com>
2019-11-09 21:24:27 +0800

    完成

--- kits.js
+++ kits.js
@@ -1,4 +1,4 @@
-function kits() {
+function Kits() {
   //获取随机整数
   this.r_Int = function(n, m) {
     return Math.floor(Math.random() * (m - n + 1) + n);
@@ -6,35 +6,18 @@ function kits() {
 
   //随机获取rgb
   this.rgb = function() {
-    var r = this.hqzs(0, 255);
-    var g = this.hqzs(0, 255);
-    var b = this.hqzs(0, 255);
+    var r = this.r_Int(0, 255);
+    var g = this.r_Int(0, 255);
+    var b = this.r_Int(0, 255);
     return "rgb(" + r + "," + g + "," + b + ")";
   };
 
   //随机16进制
-  this.jinZhi_16 = function() {
-    var arr = [
-      "0",
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "a",
-      "b",
-      "c",
-      "d",
-      "e"
-    ];
-    var color = ["#"];
-    for (var i = 0; i <= 6; i++) {
-      var r = Math.floor(Math.random() * arr.length);
-      color.push(arr[i]);
+  this.color_16 = function() {
+    var color = ["#", "c"];
+    for (var i = 1; i < 6; i++) {
+      var r = Math.floor(Math.random() * 16).toString(16);
+      color.push(r);
     }
     return color.join("");
   };
