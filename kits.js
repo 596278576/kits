d5249a43f9ce4c94a3562d650009799251c43acd
锅 <596278576@qq.com>
2019-11-14 21:26:53 +0800

    第N次上传

--- kits.js
+++ kits.js
@@ -36,4 +36,45 @@ function Kits() {
     let arr = !data ? [] : JSON.parse(data);
     return arr;
   };
+
+  // 水仙花数封装
+  // 100到1000水仙花数
+  this.narcissus = function() {
+    for (var i = 100; i < 1000; i++) {
+      //百位
+      var a = Math.floor(i / 100);
+      // 十位
+      var b = Math.floor((i - a * 100) / 10);
+      // 个位
+      var c = i % 10;
+      if (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3) === i) {
+        return console.log(i + "是一个水仙花数");
+      }
+    }
+  };
+
+  // 去掉数组的0项
+  this.removeZero = function(arr) {
+    var temp = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] !== 0) {
+        temp[temp.length] = arr[i];
+      }
+    }
+    return temp;
+  };
+
+  //冒泡排序
+  this.Bubbling = function(arr) {
+    for (var i = 0; i < arr.length - 1; i++) {
+      for (var j = 0; j < arr.length - 1 - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+          var temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+    return temp;
+  };
 }
